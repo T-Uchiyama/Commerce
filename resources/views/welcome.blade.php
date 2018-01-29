16fcda46610a95758f18ddf3017dc1037415111c
T-Uchiyama <t_uchiyama@funteam.co.jp>
2018-01-29 16:18:18 +0900

    初期画面でもAdminと通常サイトとの切り分けを実施

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,17 +66,29 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
+            @if (Request::root() == 'http://dev.commerce.com')
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
+                            <a href="{{ route('register') }}">Register</a>
+                        @endauth
+                    </div>
+                @endif
+            @elseif (Request::root() == 'http://admin.commerce.com')
+                @if (Route::has('admin.login'))
+                    <div class="top-right links">
+                        @auth('admin')
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('admin.login') }}">AdminLogin</a>
+                            <a href="{{ route('admin.register') }}">AdminRegister</a>
+                        @endauth
+                    </div>
+                @endif
             @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
